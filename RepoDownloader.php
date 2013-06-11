b5d4fc6833c0570f0bad58acdd1d8712b297d014
Jacob Kelley <jakie8@gmail.com>
2013-06-11 15:45:17 -0600

    Update RepoDownloader.php

--- RepoDownloader.php
+++ RepoDownloader.php
@@ -1,10 +1,9 @@
 <?PHP
-
 class RepoDownloader{
 
-    private function exec_redirects($ch, &$redirects) {
+    private function exec_redirects($ch, &$redirects, $die=false) {
         $data = curl_exec($ch);
-
+        
         $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         if ($http_code == 301 || $http_code == 302) {
             list($header) = explode("\r\n\r\n", $data, 2);
@@ -17,16 +16,12 @@ class RepoDownloader{
             if (isset($url_parsed)) {
                 curl_setopt($ch, CURLOPT_URL, $url);
                 $redirects++;
-                return $this->exec_redirects($ch, $redirects);
+                return $this->exec_redirects($ch, $redirects, true);
             }
         }
-
-        if ($curlopt_header) {
-            return $data;
-        } else {
-            list(, $body) = explode("\r\n\r\n", $data, 2);
-            return $body;
-        }
+        
+        list(, $body) = explode("\r\n\r\n", $data, 2);
+        return $body;
     }
 
     public function download($opts){
@@ -37,13 +32,15 @@ class RepoDownloader{
         $endpoint = 'https://api.github.com/repos/'.$user.'/'.$repo.'/zipball/master';
 
         $ch = curl_init($endpoint); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: token '.$token));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: token '.$token, 'User-Agent: GhRepoDownloader'));
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $data = $this->exec_redirects($ch, $out); 
         curl_close($ch);
-
-        file_put_contents($file, $data);
+		
+		$h = fopen($file, "a+");
+		fwrite($h, $data);
+		fclose($h);
     }
 }
